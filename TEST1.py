007dd99ab765a15b2ed71f190b93cbccdd770ba7
coxfactor <milo@coxfactor.com>
2026-03-19 17:26:55 +0900

    feat : TEST1.py test

---
+++ TEST1.py
@@ -0,0 +1,11 @@
+print ("Hello, World!")
+print ("Welcome to Python programming.")
+print ("This is a simple test script.")
+print ("Let's do some basic math:")
+a = 10
+b = 5
+print ("a + b =", a + b)
+print ("a - b =", a - b)
+print ("a * b =", a * b)
+print ("a / b =", a / b)
+print ("That's all for now. Happy coding!")
